134c28c32287defa2a91e8c1788c80722571a5c6
TiffanyMyo <timyo@students.wcpss.net>
2025-04-11 13:53:58 -0400

    Update message.py

--- Spy Games/message.py
+++ Spy Games/message.py
@@ -1,10 +1,10 @@
 def decode(text):
-    result = ''
-    for char in text:
-        if char.isalpha():
-            base = ord('A') if char.isupper() else ord('a')
-            shifted = (ord(char) - base - 3) % 26 + base
-            result += chr(shifted)
-        else:
-            result += char
-    return result
+   result = ''
+   for char in text:
+       if char.isalpha():
+           base = ord('A') if char.isupper() else ord('a')
+           shifted = (ord(char) - base - 3) % 26 + base
+           result += chr(shifted)
+       else:
+           result += char
+   return result
